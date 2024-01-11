ecf52170c5424f99ae3404ffa9137d395217b7a8
Drummer3 <drummerthree@gmail.com>
2024-01-11 22:47:28 +0400

    add new terminal keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
 
 vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search()<CR>', {
   desc = "Search on current file",
 })
+
+vim.keymap.set("n", "<leader>t", "<cmd>terminal<cr>", { desc = "Terminal" })
