b41b29b888885f4e30bf9114825773afb08cf599
David Munjishvili <d.munjishvili@livo.ge>
2024-02-15 21:55:11 +0400

    disabled nvim-lualine

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   -- { "akinsho/bufferline.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
 }
