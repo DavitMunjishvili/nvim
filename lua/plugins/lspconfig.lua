8c7ab19d528928619aec0f48fe2db376d8b2874a
David Munjishvili <d.munjishvili@livo.ge>
2024-03-02 22:26:53 +0400

    extract example.lue in separate files

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {},
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {},
+      setup = {},
+    },
+  },
+
+  -- ============ default typescript server
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   dependencies = {
+  --     "jose-elias-alvarez/typescript.nvim",
+  --     init = function()
+  --       require("lazyvim.util").lsp.on_attach(function(_, buffer)
+  --         vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+  --         vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+  --       end)
+  --     end,
+  --   },
+  --   ---@class PluginLspOpts
+  --   opts = {
+  --     ---@type lspconfig.options
+  --     servers = {
+  --       tsserver = {},
+  --     },
+  --     setup = {
+  --       tsserver = function(_, opts)
+  --         require("typescript").setup({ server = opts })
+  --         return true
+  --       end,
+  --     },
+  --   },
+  -- },
+}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
+  },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+}
