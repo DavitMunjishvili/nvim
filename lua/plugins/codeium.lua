755cba7066de91488c7c5ba57138fe7a40beeb6f
David Munjishvili <d.munjishvili@livo.ge>
2024-03-04 11:48:04 +0400

    add codeium

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({
+        sources = {
+          { name = "codeium" },
+        },
+      })
+    end,
+  },
+}
